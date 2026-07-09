4108b2da784457e9a06dccb0565a8e1b1bb30a39
lionel.hervier@laposte.net <lionel.hervier@laposte.net>
2026-07-09 23:41:32 +0200

    Nettoie le AssemblyInfo

--- Src/AssemblyInfo.cs
+++ Src/AssemblyInfo.cs
@@ -1,6 +1,5 @@
 using System.Reflection;
 
-[assembly: AssemblyTitle("EvaCMGroundMod")]
 [assembly: AssemblyDescription("Mod disabling the positionning of parts in the ground when in EVA construction mode")]
 [assembly: AssemblyConfiguration("")]
 [assembly: AssemblyCompany("")]
